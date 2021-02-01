db154edaa4fdd27cc0aecd95000a2ef1796a3ca9
vriznet <vriz@vriz.net>
2021-02-01 09:35:18 +0900

    Loading Posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { PostAuthor } from './PostAuthor';
 import { ReactionButtons } from './ReactionButtons';
+import { selectAllPosts, fetchPosts } from './postsSlice';
 
 export const PostsList = () => {
-  const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+  const posts = useSelector(selectAllPosts);
+
+  const postStatus = useSelector((state) => state.posts.status);
+
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts());
+    }
+  }, [postStatus, dispatch]);
+
   const orderedPosts = posts
     .slice()
     .sort((a, b) => b.date.localeCompare(a.date));
