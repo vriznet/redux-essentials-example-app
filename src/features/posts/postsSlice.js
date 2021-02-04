77f6065b9b349bae0390016c3394644bf573e384
vriznet <vriz@vriz.net>
2021-02-04 15:10:45 +0900

    Solving an UserPage Re-render Problem

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,8 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import {
+  createSlice,
+  createAsyncThunk,
+  createSelector,
+} from '@reduxjs/toolkit';
 import { client } from '../../api/client';
 
 const initialState = {
@@ -66,3 +70,8 @@ export const selectAllPosts = (state) => state.posts.posts;
 
 export const selectPostById = (state, postId) =>
   state.posts.posts.find((post) => post.id === postId);
+
+export const selectPostsByUser = createSelector(
+  [selectAllPosts, (state, userId) => userId],
+  (posts, userId) => posts.filter((post) => post.user === userId)
+);
